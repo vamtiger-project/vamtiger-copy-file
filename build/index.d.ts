231a87ee1fe400f49265109812ed324c72967ca0
James-Alex Matthews <vamtiger@vamtiger.com>
2017-12-17 22:43:15 +0200

    vamtiger-node-typescript-commit

--- build/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { PathLike } from 'fs';
-declare const _default: (params: Params) => Promise<{}>;
-export default _default;
-export interface Params {
-    source: PathLike;
-    destination: PathLike;
-}
